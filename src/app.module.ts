45bb42c9550d0613db65dfb2cc122319d8be3d8a
pallishree-saswat <saswataeroarya@gmail.com>
2022-03-29 09:52:49 +0530

    upload files in nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,47 @@
-import { Module ,NestModule, MiddlewareConsumer, RequestMethod} from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserController } from './users/user.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { BlogsService } from './blogs/blogs.service';
 
-import {AuthMiddleware} from './middleware/auth'
+import { AuthMiddleware } from './middleware/auth';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './exception/http.filter';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [EmployeeModule],
-  controllers: [AppController,UserController],
-  providers: [AppService, BlogsService,{
-    provide: APP_FILTER,
-    useClass:HttpExceptionFilter
-  }],
+  imports: [
+    EmployeeModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
+  controllers: [AppController, UserController],
+  providers: [
+    AppService,
+    BlogsService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 // export class AppModule {}
-
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware)
-    //.forRoutes('posts')
-    // .forRoutes(UserController)
-    .forRoutes({
-      path:'posts/post', method:RequestMethod.GET
-    })
+    consumer
+      .apply(AuthMiddleware)
+      //.forRoutes('posts')
+      // .forRoutes(UserController)
+      .forRoutes({
+        path: 'posts/post',
+        method: RequestMethod.GET,
+      });
   }
 }
